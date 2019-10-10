21d20a719baff91e8c55890a43d9465043418a80
Elliott Thompson <ellth92@gmail.com>
2019-10-10 17:07:49 +0100

    added support for colour and grayscale output

--- main.py
+++ main.py
@@ -35,6 +35,11 @@ parser.add_argument('--size', help='size of the Bayer matrix (optional, default=
 parser.add_argument('--range', help='range of dithering spread (optional, default=8)')
 parser.add_argument('--offset', help='brightness offset, multiple of range (optional, default=0)')
 
+def grayscale_argument(v):
+    return True
+
+parser.add_argument('--grayscale', nargs='?', type=grayscale_argument, const=True, default=False, help='convert output to grayscale (optional, default=False)')
+
 args=parser.parse_args()
 
 filename = args.image
@@ -44,13 +49,18 @@ if filename is None:
 n = int(args.size or 4)
 r = int(args.range or 8)
 offset = (1 / r) * int(args.offset or 0)
+grayscale = args.grayscale
 
-image = imread("{}/{}".format(
+image = imread("{}/src/{}".format(
     PATH, filename
 ))
-image = rgb2gray(image)
-output = np.zeros(image.shape)
 
+if grayscale:
+    image = rgb2gray(image)
+else:
+    image = image / 256
+
+output = np.zeros(image.shape)
 
 M = np.zeros([n*2,n*2])
 
@@ -62,18 +72,30 @@ for i in range(0, n*2):
 
 for i in range(0, image.shape[0]):
     for j in range(0, image.shape[1]):
-        image_val = image[i][j]
-        matrix_val = M[i % (n*2)][j % (n*2)]
-        curr_range = 1 / r
-        temp = image_val + (curr_range * matrix_val)
-        output[i][j] = (round(temp * (1 / curr_range)) / (1 / curr_range)) - offset
+        if len(image.shape) == 2:
+            image_val = image[i][j]
+            matrix_val = M[i % (n*2)][j % (n*2)]
+            curr_range = 1 / r
+            temp = image_val + (curr_range * matrix_val)
+            output[i][j] = (round(temp * (1 / curr_range)) / (1 / curr_range)) - offset
+        elif len(image.shape) == 3:
+            for c in range(0, 3):
+                image_val = image[i][j][c]
+                matrix_val = M[i % (n*2)][j % (n*2)]
+                curr_range = 1 / r
+                temp = image_val + (curr_range * matrix_val)
+                output[i][j][c] = (round(temp * (1 / curr_range)) / (1 / curr_range)) - offset
 
 if not os.path.exists('bin'):
     os.makedirs('bin')
-imsave("{}/bin/dither_n-{}_r-{}_o-{}_{}".format(
+
+curr_cmap = 'gray' if grayscale else None
+
+imsave("{}/bin/dither_n-{}_r-{}_o-{}_g-{}_{}".format(
     PATH,
     n,
     r,
     offset,
+    grayscale,
     filename
-), np.clip(output, 0, 1), cmap='gray')
+), np.clip(output, 0, 1), cmap=curr_cmap)
--- readme.md
+++ readme.md
@@ -1,6 +1,6 @@
 # Ordered Dithering Python Script
 
-Produces an ordered dithering effect for the supplied '.jpg' image.
+Produces an ordered dithering effect for the supplied '.jpg' image. Supplied images must be placed within a './src' folder at the root of this projects directory.
 
 Basic usage:
 'python3 main.py --image test.jpg'
